60bae3fd15eb044a7ec7bb08afe9f7bf74504f1a
Victor Simiao <victor.sreis@hotmail.com>
2021-02-19 17:59:02 -0300

    Atualizando os Relacionamentos

--- src/main/java/br/com/victorreis/springdata/model/Funcionario.java
+++ src/main/java/br/com/victorreis/springdata/model/Funcionario.java
@@ -2,11 +2,16 @@ package br.com.victorreis.springdata.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -35,7 +40,15 @@ public class Funcionario {
 	private LocalDate dataContratacao = LocalDate.now();
 
 	@ManyToOne
+	@JoinColumn(name = "cargo_id", nullable = false)
 	private Cargo cargo;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "funcionarios_unidades",
+		joinColumns =
+		{@JoinColumn(name="fk_funcionario")},
+		inverseJoinColumns = {@JoinColumn(name="fk_unidade")})
+	private List<UnidadeTrabalho> unidadesTrabalho;
 
 	public Funcionario() {
 
